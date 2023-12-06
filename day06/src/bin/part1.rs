1ffddc71d96091a8052d2a3b55bd86785b253932
Jonas Johansson <contact@jonas.johansson.dev>
2023-12-06 07:47:19 +0100

    day 6 part 1

--- day06/src/bin/part1.rs
+++ day06/src/bin/part1.rs
@@ -4,9 +4,31 @@ fn main() {
     dbg!(output);
 }
 
-fn solve(input: &str) -> String {
-    let output = input;
+fn solve(_input: &str) -> String {
+    let mut output = 1;
+    let times = [40,     81,     77,     72];
+    let distance = [219,   1012,   1365,   1089];
+    let mut stuff = vec![];
+    for i in 0..4 {
+        stuff.push(calculate_above(times[i], distance[i]));
+    }
+    for x in stuff {
+        output *= x;
+    }
     output.to_string()
+}
+
+fn calculate_above (time: u32, distance: u32) -> u32 {
+    let mut times = 0;
+    for x in 0..time  {
+        let hold_down = x;
+        let dist = hold_down * (time - x);
+        //dbg!(dist);
+        if dist > distance {
+            times += 1;
+        }
+    }
+    times
 }
 
 #[cfg(test)]
@@ -21,9 +43,15 @@ Distance:  9  40  200");
     }
 
     #[test]
+    fn calculate_distance() {
+        let result = calculate_above(71530, 940200);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
     fn solve_actual_puzzle() {
         let result = solve("Time:        40     81     77     72
         Distance:   219   1012   1365   1089");
-        assert_eq!(result, result);
+        assert_eq!(result, "861300");
     }
 }
